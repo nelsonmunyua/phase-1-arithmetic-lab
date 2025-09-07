a7f91ac5064a408fb2a31459308b1c30980484d1
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-09-07 20:51:57 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+//return a random number > 1
+const random = Math.ceil(Math.random())
+//Math.ceil(random)
+console.log(random)
+
+// do a modulus and return 4
+const num3 = 8;
+const num4 = 2;
+const mod = num3 / num4;
+console.log(mod);
+
+//return the highest number as 25
+const max = 20;
+Math.max(1,4,6,20);
+console.log(Math.max(1,4,6,20));
+
